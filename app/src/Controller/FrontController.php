75099a990e12104159ce7eadee903c0f8c10ad00
simonBtssio <simon.farnault.sio@gmail.com>
2023-03-28 11:25:46 +0200

    auth + login

--- app/src/Controller/FrontController.php
+++ app/src/Controller/FrontController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TestController extends AbstractController
+class FrontController extends AbstractController
 {
-    #[Route('/test', name: 'app_test')]
+    #[Route('/front', name: 'app_front')]
     public function index(): Response
     {
-        return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
         ]);
     }
 }
