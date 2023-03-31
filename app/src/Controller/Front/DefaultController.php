89c0cffa7b4c6acb556d975e072e9c56387b8c8f
Maxime <maxime.laotonlop@gmail.com>
2023-03-31 15:54:01 +0200

    Main page for members

--- app/src/Controller/Front/DefaultController.php
+++ app/src/Controller/Front/DefaultController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\UserRepository;
+use App\Repository\ArticleRepository;
+use App\Repository\VideoRepository;
 use Symfony\Component\Security\Core\User\UserInterface;
 use App\Entity\User;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -15,11 +17,13 @@ use App\Form\FrontUserType;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default_index')]
-    public function index(UserRepository $userRepository, UserInterface $user): Response
+    public function index(UserRepository $userRepository, UserInterface $user, ArticleRepository $articleRepository, VideoRepository $videoRepository): Response
     {
         return $this->render('front/default/index.html.twig', [
             'user' => $userRepository->findBy(array('id' => $user->getId())),
             'user_tag' => $user->getTags(),
+            'articles' => $articleRepository->findAll(),
+            'videos' => $videoRepository->findAll(),
         ]);
     }
 
